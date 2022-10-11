29b6022d5697fd44a3be0fca8369a798b4810b1f
Echi0905 <werckmeister0905@gmail.com>
2022-10-11 18:03:22 +0900

    商品出品機能・テストコード⑥

--- app/models/product.rb
+++ app/models/product.rb
@@ -25,9 +25,8 @@ class Product < ApplicationRecord
   validates :shipping_cost_id,  presence: true
   validates :prefecture_id,     presence: true
   validates :shipping_day_id,   presence: true    
-  validates :price,             presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  # ジャンルの選択が「--」の時は保存不可
   with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :prefecture_id
