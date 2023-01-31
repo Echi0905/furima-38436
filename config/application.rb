676ef6234e3944ee044a9db0be3b495dc3c3efb1
Echi0905 <werckmeister0905@gmail.com>
2023-01-31 11:46:07 +0900

    デプロイ

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Furima38436
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.hosts << 'xxx.onrender.com'
   end
 end
